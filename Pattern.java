37a166c344adbc22820d814ac2a5c7960989b125
TEJASWINI N NAIK <tejaswini.n.naik@gmail.com>
2025-12-01 11:01:28 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,22 @@
+class Pattern {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+        System.out.println("Right Half pyramid");
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+        System.out.println("Reverse pyramid");
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+        System.out.println("Left Half pyramid");
+    }
+}
