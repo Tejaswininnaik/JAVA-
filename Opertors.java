3943f2e49e4d4aa0a49cd197bb0736fcd2d64e04
TEJASWINI N NAIK <tejaswini.n.naik@gmail.com>
2025-12-03 22:23:22 +0530

    Add files via upload

---
+++ Opertors.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Opertors {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = 4;
+        int b = 5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+        System.out.println(a%b);
+    }
+}
