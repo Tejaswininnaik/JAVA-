99a3a28cf6bbb03e6ff5852a39499d08fa3f1323
TEJASWINI N NAIK <tejaswini.n.naik@gmail.com>
2025-12-04 23:06:08 +0530

    Add files via upload

---
+++ Floating.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Floating {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a = 5;
+        double b = 6;
+        System.out.println(a*b);
+    }
+}
