589181360517d4ff7960e4583053ccb27b6a1239
TEJASWINI N NAIK <tejaswini.n.naik@gmail.com>
2025-12-05 21:50:04 +0530

    Add files via upload

---
+++ Oddeven.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Oddeven {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int num = input.nextInt();
+        if(num%2==0){
+            System.out.println("Number is even");
+        }
+        else {
+            System.out.println("Number is odd");
+        }
+    }
+}
