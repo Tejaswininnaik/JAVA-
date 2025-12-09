60797d1811dcc5902212e94d779a1c4e7c0aa003
TEJASWINI N NAIK <tejaswini.n.naik@gmail.com>
2025-12-09 23:53:27 +0530

    Add files via upload

---
+++ rightshift.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class rightshift {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.println("Showcasing Bitwise XOR operator\n");
+        System.out.print("Enter your number:");
+        int num1 = input.nextInt();
+        int number = num1 >> 4;
+        System.out.println("The number is " + number);
+    }
+}
